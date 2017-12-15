8974ef07137a6f57068147f108b06f92cec31025
Chris Klanac <cklanac@users.noreply.github.com>
2017-12-15 12:24:00 -0600

    Promisify db and routes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Listful App
 ============================
+
+Update client api.js. move from $ajax to fetch
+Use promises .then() and .catch()
+
+promisify simDB
+update endpoints
+
+promisify app.listen and server.close
--- public/js/app.js
+++ public/js/app.js
@@ -7,10 +7,12 @@ const render = new Render(store, api);
 
 $(function () {
 
-  api.search({}, response => {
-    store.data = response;
-    render.shoppingList();
-  });
+  api.search()
+    .then(response => {
+      store.data = response;
+      render.shoppingList();
+    })
+    .catch(console.error);
 
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
@@ -18,11 +20,13 @@ $(function () {
     const inputForm = $(event.currentTarget);
     const newItemInput = inputForm.find('input[name=shopping-list-input]');
 
-    api.create({ name: newItemInput.val() }, response => {
-      newItemInput.val('');
-      store.data.unshift(response);
-      render.shoppingList();
-    });
+    api.create({ name: newItemInput.val() })
+      .then(response => {
+        newItemInput.val('');
+        store.data.unshift(response);
+        render.shoppingList();
+      })
+      .catch(console.error);
   });
 
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
@@ -30,32 +34,38 @@ $(function () {
     const id = $(event.currentTarget).closest('.js-item-id-element').attr('data-item-id');
     const item = store.findById(id);
 
-    api.update(id, { checked: !item.checked }, response => {
-      item.checked = response.checked;
-      render.shoppingList();
-    });
+    api.update(id, { checked: !item.checked })
+      .then(response => {
+        item.checked = response.checked;
+        render.shoppingList();
+      })
+      .catch(console.error);
   });
 
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     event.preventDefault();
     const id = $(event.currentTarget).closest('.js-item-id-element').attr('data-item-id');
 
-    api.remove(id, () => {
-      store.findByIdAndRemove(id);
-      render.shoppingList();
-    });
+    api.remove(id)
+      .then(() => {
+        store.findByIdAndRemove(id);
+        render.shoppingList();
+      })
+      .catch(console.error);
   });
 
   $('.js-shopping-list').on('change', '.js-shopping-item', event => {
     event.preventDefault();
     const id = $(event.currentTarget).closest('.js-item-id-element').attr('data-item-id');
     const itemUpdate = { name: $(event.currentTarget).val() };
-    console.log(itemUpdate)
 
-    api.update(id, itemUpdate, () => {
-      store.findByIdAndUpdate(id, itemUpdate);
-      render.shoppingList();
-    });
+    api.update(id, itemUpdate)
+      .then(() => {
+        store.findByIdAndUpdate(id, itemUpdate);
+        render.shoppingList();
+      })
+      .catch(console.error);
+
   });
 
 });
