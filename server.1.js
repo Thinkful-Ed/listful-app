5d87841f56c1f779eb24a40fb2dc4e08d32c83b9
Chris Klanac <cklanac@users.noreply.github.com>
2017-12-18 12:15:45 -0600

    add hello world server

--- server.1.js
+++ server.1.js
@@ -1,8 +1,18 @@
-// const hostname = '127.0.0.1';
-// const port = 8080;
+'use strict';
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<b>Hello World</b>');
-// });
+// Simple Webserver in Node 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
